51e3b62a88aea17bffd49faa94b3066ea3b7701f
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-12-30 20:03:07 +0100

    fix for json serialization of MostConsumedFoodDto ⚒

--- model/dto/MostConsumedFoodDto.go
+++ model/dto/MostConsumedFoodDto.go
@@ -3,9 +3,9 @@ package dto
 import "github.com/google/uuid"
 
 type MostConsumedFoodDto struct {
-	FoodId          uuid.UUID
-	FoodName        string
-	QuantityUsed    float32
-	QuantityUsedStd float32
-	Unit            string
+	FoodId          uuid.UUID 		`json:"foodId"`
+	FoodName        string 			`json:"foodName"`
+	QuantityUsed    float32 		`json:"quantityUsed"`
+	QuantityUsedStd float32 		`json:"quantityUsedStd"`
+	Unit            string 			`json:"unit"`
 }
